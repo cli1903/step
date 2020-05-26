34d5ee47419017ec8b51edd521993709b7476495
Cindy Li <cindy_li@brown.edu>
2020-05-26 13:20:09 -0400

    went through walkthrough, added contact info section

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -19,10 +19,15 @@ function addRandomGreeting() {
   const greetings =
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
+  const facts = 
+      ['I live in NY.', 'I have a yellow lab named Coco.', 'I hate the cold.', 'I like reading.'];
+
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
+  const fact = facts[Math.floor(Math.random() * facts.length)];
+
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  greetingContainer.innerText = fact;//greeting;
 }
