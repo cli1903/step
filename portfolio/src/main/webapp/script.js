f1b467c967bbeb7e7129ed05014fd8c183a5e707
Cindy Li <cindy_li@brown.edu>
2020-05-29 09:47:29 -0400

    added images to images page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,30 +15,26 @@
 /**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  const visit = 
+function addRandomDestination() {
+  const placesToVisit = 
       ['Tokyo', 'London', 'Rome', 'Amsterdam', 'Taipei', 'Sichuan'];
 
   const visit_pics = {'Tokyo': 'https://cdn.pixabay.com/photo/2020/01/15/19/03/shinjuku-4768674_960_720.jpg',
     'London': 'https://c1.wallpaperflare.com/preview/271/1022/559/westminster-palace-london-city.jpg',
     'Rome': 'https://cdn.pixabay.com/photo/2018/07/20/14/02/rome-3550739_960_720.jpg', 'Amsterdam': 'https://upload.wikimedia.org/wikipedia/commons/e/e8/Amsterdam_De_Wallen_6.jpg', 'Taipei': 'https://upload.wikimedia.org/wikipedia/commons/c/cc/Taipei_skyline_cityscape_at_night_with_full_moon.jpg', 'Sichuan': 'https://www.goodfreephotos.com/albums/china/sichuan/other/Guangfu-pavilion-with-summit-visible-in-background-in-mount-emei-sichuan-china.jpg'}
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  // Pick a random travel destination
 
-  const fact = visit[Math.floor(Math.random() * visit.length)];
+  const destination = placesToVisit[Math.floor(Math.random() * placesToVisit.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
+  const travelContainer = document.getElementById('travel-container');
   
 
   const img = document.getElementById('visit-pic');
-  img.src = visit_pics[fact];
-  img.alt = fact + '-image';
+  img.src = visit_pics[destination];
+  img.alt = destination + '-image';
 
-  greetingContainer.innerText = fact;
+  travelContainer.innerText = destination;
   
 }
