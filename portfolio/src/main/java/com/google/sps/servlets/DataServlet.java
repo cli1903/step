3fc292b7ab40685430027347970208ffc8a8d94c
Cindy Li <cindy_li@brown.edu>
2020-06-03 19:51:13 +0000

    add datastore dependencies

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,13 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.sps.data.Comment;
 import java.util.ArrayList;
 import java.util.List;
 import com.google.gson.Gson;
@@ -69,5 +76,4 @@ public class DataServlet extends HttpServlet {
     response.setContentType("appplication/json;");
     response.getWriter().println(json);
   }
-}
 }
