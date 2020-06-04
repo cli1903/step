7f243e7ccb8f06fb4da5af384c52baf811fa3b68
Cindy Li <cindy_li@brown.edu>
2020-06-04 21:22:38 +0000

    fix formatting issues

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,31 +16,32 @@
  * Adds a random travel destination and image of destination to the page.
  */
 function addRandomDestination() {
-  const placesToVisit = 
+  const placesToVisit =
       ['Tokyo', 'London', 'Rome', 'Amsterdam', 'Taipei', 'Sichuan'];
 
   const visitPics = {
-    'Tokyo': 'https://cdn.pixabay.com/photo/2020/01/15/19/03/' + 
-             'shinjuku-4768674_960_720.jpg',
-    'London': 'https://c1.wallpaperflare.com/preview/271/1022/559/' + 
-              'westminster-palace-london-city.jpg',
-    'Rome': 'https://cdn.pixabay.com/photo/2018/07/20/14/02/' + 
-            'rome-3550739_960_720.jpg', 
-    'Amsterdam': 'https://upload.wikimedia.org/wikipedia/commons/e/e8/' + 
-                 'Amsterdam_De_Wallen_6.jpg', 
-    'Taipei': 'https://upload.wikimedia.org/wikipedia/commons/c/cc/' + 
-              'Taipei_skyline_cityscape_at_night_with_full_moon.jpg', 
-    'Sichuan': 'https://www.goodfreephotos.com/albums/china/sichuan/other/' + 
-               'Guangfu-pavilion-with-summit-visible-in-' + 
-               'background-in-mount-emei-sichuan-china.jpg'
+    'Tokyo': 'https://cdn.pixabay.com/photo/2020/01/15/19/03/' +
+        'shinjuku-4768674_960_720.jpg',
+    'London': 'https://c1.wallpaperflare.com/preview/271/1022/559/' +
+        'westminster-palace-london-city.jpg',
+    'Rome': 'https://cdn.pixabay.com/photo/2018/07/20/14/02/' +
+        'rome-3550739_960_720.jpg',
+    'Amsterdam': 'https://upload.wikimedia.org/wikipedia/commons/e/e8/' +
+        'Amsterdam_De_Wallen_6.jpg',
+    'Taipei': 'https://upload.wikimedia.org/wikipedia/commons/c/cc/' +
+        'Taipei_skyline_cityscape_at_night_with_full_moon.jpg',
+    'Sichuan': 'https://www.goodfreephotos.com/albums/china/sichuan/other/' +
+        'Guangfu-pavilion-with-summit-visible-in-' +
+        'background-in-mount-emei-sichuan-china.jpg'
   }
 
   // Pick a random travel destination.
-  const placeToVisit = placesToVisit[Math.floor(Math.random() * placesToVisit.length)];
+  const placeToVisit =
+      placesToVisit[Math.floor(Math.random() * placesToVisit.length)];
 
   // Add it to the page.
   const travelContainer = document.getElementById('travel-container');
-  
+
 
   const img = document.getElementById('visit-pic');
 
@@ -51,21 +52,21 @@ function addRandomDestination() {
 }
 
 
-/** 
+/**
  * adds response from servlet
  */
 
 function setComments() {
-    fetch('/data').then((response) => response.json()).then((obj) => {
-        commentContainer = document.getElementById('comments-container');
-        for (let i = 0; i < obj.length; i++) {
-            commentContainer.appendChild(createListElem(obj[i]));
-        }
-    });
+  fetch('/data').then((response) => response.json()).then((obj) => {
+    commentContainer = document.getElementById('comments-container');
+    for (let i = 0; i < obj.length; i++) {
+      commentContainer.appendChild(createListElem(obj[i]));
+    }
+  });
 }
 
 function createListElem(text) {
-    const liElem = document.createElement('li');
-    liElem.innerText = text;
-    return liElem;
+  const liElem = document.createElement('li');
+  liElem.innerText = text;
+  return liElem;
 }
