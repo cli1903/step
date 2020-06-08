3d3fdc977e495614875883607ba1a16de88e19b4
cli1903 <47994462+cli1903@users.noreply.github.com>
2020-06-08 16:16:24 -0400

    add private final to datastore and gson variables

--- portfolio/src/main/java/com/google/sps/servlets/DataDeleteServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataDeleteServlet.java
@@ -34,8 +34,8 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/delete-data")
 public class DataDeleteServlet extends HttpServlet {
-  DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-  Gson gson = new Gson();
+  private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+  private final Gson gson = new Gson();
 
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     Query commentQuery = new Query("Comment");
