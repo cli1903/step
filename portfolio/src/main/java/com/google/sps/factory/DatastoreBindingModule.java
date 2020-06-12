787c0cdb92313871fde180845a35d546edcaaa12
cli1903 <47994462+cli1903@users.noreply.github.com>
2020-06-12 09:37:31 -0400

    Update DatastoreBindingModule.java

--- portfolio/src/main/java/com/google/sps/factory/DatastoreBindingModule.java
+++ portfolio/src/main/java/com/google/sps/factory/DatastoreBindingModule.java
@@ -6,7 +6,6 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 
-
 public class DatastoreBindingModule extends AbstractModule {
   @Provides
   @Singleton
