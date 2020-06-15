17a52be2fa1d9019e03c45e865c16ad434e72f9b
Cindy Li <cindy_li@brown.edu>
2020-06-15 21:49:33 +0000

    fix formatting

--- portfolio/src/main/java/com/google/sps/storage/DatastoreCommentStorage.java
+++ portfolio/src/main/java/com/google/sps/storage/DatastoreCommentStorage.java
@@ -76,10 +76,6 @@ public class DatastoreCommentStorage implements CommentStorage {
     String text = (String) entity.getProperty(ENTITY_TEXT_PARAM);
     long timePosted = (long) entity.getProperty(ENTITY_TIME_PARAM);
 
-    return Comment.builder
-      .setName(name)
-      .setText(text)
-      .setTimePosted(timePosted)
-      .build();
+    return Comment.builder.setName(name).setText(text).setTimePosted(timePosted).build();
   }
 }
