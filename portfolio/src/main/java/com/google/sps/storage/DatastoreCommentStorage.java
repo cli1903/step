54c031211230ad9253ac07534d59235436ca0b63
cli1903 <47994462+cli1903@users.noreply.github.com>
2020-06-15 09:35:31 -0400

    Update DatastoreCommentStorage.java

--- portfolio/src/main/java/com/google/sps/storage/DatastoreCommentStorage.java
+++ portfolio/src/main/java/com/google/sps/storage/DatastoreCommentStorage.java
@@ -76,10 +76,10 @@ public class DatastoreCommentStorage implements CommentStorage {
     String text = (String) entity.getProperty(ENTITY_TEXT_PARAM);
     long timePosted = (long) entity.getProperty(ENTITY_TIME_PARAM);
 
-    Comment.Builder commentBuilder = Comment.builder();
-
-    Comment comment = commentBuilder.setName(name).setText(text).setTimePosted(timePosted).build();
-
-    return comment;
+    return Comment.builder
+      .setName(name)
+      .setText(text)
+      .setTimePosted(timePosted)
+      .build();
   }
 }
